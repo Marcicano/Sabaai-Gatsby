feef73aa48ca1fc7723366d65ba6067222d35458
romene <rogerio.eire@gmail.com>
2019-04-18 20:06:09 +1200

    createPage api

--- src/components/IndexComponents/Cursos.js
+++ src/components/IndexComponents/Cursos.js
@@ -1,21 +1,66 @@
 import React from 'react'
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 import ImageTeste from "../../images/cursos/formacao.png"
 
 
 const Cursos = () => {
+  const data = useStaticQuery(graphql`
+  query {
+  allMarkdownRemark {
+    edges{
+      node{
+        id
+        fields {
+          slug
+        }
+        frontmatter{
+          title
+          thumbnail
+          date
+          cidadeCurso
+          publicoCurso
+          sobreCurso
+          valorVista
+          valorParcelado
+        }
+      }
+    }
+  }
+}
+  `)
+
+  
+  return (
+    <React.Fragment>
+      {data.allMarkdownRemark.edges.map(item => {
+        return <Curso key={item.node.id} curso={item.node} />
+      })}
+      
+    </React.Fragment>
+  )
+}
+
+export default Cursos
+
+
+
+
+const Curso = ({curso}) => {
+  console.log(curso.fields.slug)
+  const { thumbnail, title, cidadeCurso } = curso.frontmatter
+  const { slug } = curso.fields
   return (
     <React.Fragment>
       <section className="gallery" id="cursos">
         <div className="row">
           <div className="col-xs-12 col-md-6">
             <div className="gallery-item col-xs-12 ">
-              <Link to="">
-                <img src={ImageTeste} alt="" className="curso-image center-image" />
+              <Link to={`/cursos/${slug}`}>
+                <img src={thumbnail} alt="" className="curso-image center-image" />
                 <div className="row middle-xs center-xs overlay">
                   <div className="col-xs-12 cursos-text">
-                    <h3>Curso</h3>
-                    <h4>cidadeCurso</h4>
+                    <h3>{title}</h3>
+                    <h4>{cidadeCurso}</h4>
                   </div>
                 </div>
               </Link>
@@ -26,22 +71,6 @@ const Cursos = () => {
     </React.Fragment>
   )
 }
-
-export default Cursos
-
-
-
-
-
-// Ativar esse component quando fizer o loop
-
-// const Curso = () => {
-//   return (
-//     <div>
-      
-//     </div>
-//   )
-// }
 
 
 
---
+++ src/templates/cursosTemplate.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import { graphql } from "gatsby";
+
+export const queryPage = graphql`
+    query Pagebypath (
+  $slug : String
+){
+  markdownRemark (
+    fields: {
+      slug:{
+        eq: $slug
+      }
+    }
+  ) {
+    frontmatter {
+     title
+     publicoCurso
+      sobreCurso
+      abordagemCurso
+      valorVista
+      valorParcelado
+      thumbnail
+    }
+  }
+}
+`
+
+
+const internalPage = ({data}) => {
+    console.log(data.markdownRemark.frontmatter.title)
+    const { title, publicoCurso, sobreCurso, abordagemCurso, valorVista, valorParcelado } = data.markdownRemark.frontmatter
+  return (
+      <React.Fragment>
+
+          <div>
+              <div className="col page-title">
+                  <h1>  {title} </h1>
+              </div>
+              <div className="inscrevase-container">
+                  <form id="preinscricao">
+                      <input type="hidden" name="preinscricao" defaultValue />
+                      <input type="hidden" name="curso" defaultValue />
+                      <div className="row">
+                          <div className="col-xs-12">
+                              Inscreva-se já! Demora menos de 2 minutos.
+              </div>
+                          <div className="col-xs-12 col-sm-9">
+                              <input
+                                  type="email"
+                                  title="email"
+                                  name="email"
+                                  placeholder="Informe-nos seu email!"
+                              />
+                          </div>
+                          <div className="col-xs-12 col-sm-3">
+                              <input type="submit" defaultValue="Inscreva-se!" />
+                          </div>
+                      </div>
+                  </form>
+              </div>
+          </div>
+          <div className="container page-section">
+              <div className="col">
+                  <h2 className="page-section">A quem é destinado este curso?</h2>
+                  <p> {publicoCurso} </p>
+                  <h2>Sobre o curso</h2>
+                  <p> {sobreCurso} </p>
+
+                  <h2>O que será abordado nas aulas</h2>
+                  <p> {abordagemCurso} </p>
+                  <h2>Valores</h2>
+                  <ul>
+                      <li>
+                          <p>{valorVista}</p>
+                      </li>
+                      <li>
+                          <p>{valorParcelado}</p>
+                      </li>
+                  </ul>
+              </div>
+          </div>
+      </React.Fragment>
+  )
+}
+
+export default internalPage
