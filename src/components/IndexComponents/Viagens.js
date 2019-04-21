6182f7db5d19c364d2e84aad4d26e6f6dd924e01
romene <rogerio.eire@gmail.com>
2019-04-21 20:45:05 +1200

    graphql viagens collection

--- src/components/IndexComponents/Viagens.js
+++ src/components/IndexComponents/Viagens.js
@@ -12,27 +12,19 @@ const Viagens = () => {
       ) {
         edges {
           node {
-            frontmatter {
-              viagemKey
-              title
-              content
-            }
+            html
           }
         }
       }
     }
   `)
-  const viagem = data.allMarkdownRemark.edges[0].node.frontmatter
+  const viagem = data.allMarkdownRemark.edges[0].node.html
   return (
     <React.Fragment>
       <section className="about" id="viagem">
         <div className="container middle-xs ">
           <div className="row content">
-            <div className="col-xs-12 middle-xs whitebox">
-              <h2>{viagem.title}</h2>
-              {/* <p>{viagem.content}</p> */}
-              <div dangerouslySetInnerHTML={{ __html: viagem.content }} />
-            </div>
+            <div className="col-xs-12 middle-xs whitebox" dangerouslySetInnerHTML={{__html: viagem}} />
             <div className="col-xs-12 col-sm-9 viagem-link ">
               <br />
               <Link to="">Faça sua pré inscrição agora clicando aqui!</Link>
